b8d48f8c5daeae2e2ff1a5bd339bd9028bdeed6d
Danilo <icf.danilo.grujicic@gmail.com>
2017-08-28 10:52:27 +0200

    Replaced from Model to Entities

--- DailyPlanning/Infrastructure/Entities/DailyPlan.cs
+++ DailyPlanning/Infrastructure/Entities/DailyPlan.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace DailyPlanning.Models
+namespace DailyPlanning.Infrastructure.Entities
 {
     public class DailyPlan
     {
