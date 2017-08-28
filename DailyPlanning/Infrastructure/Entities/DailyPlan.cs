bf1305e584d943b0a60ae8769986859df35d2934
Danilo <icf.danilo.grujicic@gmail.com>
2017-08-28 11:56:53 +0200

    Added Many to Many relationship with WorkItem; added data annotations

--- DailyPlanning/Infrastructure/Entities/DailyPlan.cs
+++ DailyPlanning/Infrastructure/Entities/DailyPlan.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
@@ -8,6 +9,14 @@ namespace DailyPlanning.Infrastructure.Entities
 {
     public class DailyPlan
     {
+        public DailyPlan()
+        {
+            this.DayBefore = new Collection<WorkItem>();
+            this.Today = new Collection<WorkItem>();
+        }
+
+        [Key]
+        public int DailyPlanID { get; set; }
         [Required]
         public DateTime Date { get; set; }
         public ICollection<WorkItem> DayBefore { get; set; }
@@ -15,6 +24,6 @@ namespace DailyPlanning.Infrastructure.Entities
         public ICollection<WorkItem> Today { get; set; }
         [StringLength(500)]
         public string Note { get; set; }
-
+        
     }
 }
--- DailyPlanning/Infrastructure/Entities/WorkItem.cs
+++ DailyPlanning/Infrastructure/Entities/WorkItem.cs
@@ -1,4 +1,6 @@
 ﻿using DailyPlanning.Infrastructure.Enums;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 
 namespace DailyPlanning.Infrastructure.Entities
 {
@@ -6,11 +8,14 @@ namespace DailyPlanning.Infrastructure.Entities
     {
         public WorkItem()
         {
+            this.DailyPlans = new Collection<DailyPlan>();
         }
         public int WorkItemID { get; set; }
         public string Description { get; set; }
         public Status Status { get; set; }
         public int ProjectID { get; set; }
         public virtual Project Project { get; set; }
+
+        public ICollection<DailyPlan> DailyPlans { get; set; }
     }
 }
