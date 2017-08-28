d6a952f971202023eb26d2759cc863d21fa33fb5
Alisa <alisa.macos@gmail.com>
2017-08-28 10:44:15 +0200

    Create entities for Projects and WorkItems;

---
+++ DailyPlanning/Infrastructure/Entities/Project.cs
@@ -0,0 +1,11 @@
+﻿namespace DailyPlanning.Infrastructure.Entities
+{
+    public class Project
+    {
+        public Project()
+        {
+        }
+        public int ProjectID { get; set; }
+        public string Title { get; set; }
+    }
+}
