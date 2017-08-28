e7d57b8f8e8ea58094eb23014cd4f681028647f2
Alisa <alisa.macos@gmail.com>
2017-08-28 11:42:48 +0200

    Add Data Annotations to Project and WorkItem;

--- DailyPlanning/Infrastructure/Entities/Project.cs
+++ DailyPlanning/Infrastructure/Entities/Project.cs
@@ -1,11 +1,18 @@
-﻿namespace DailyPlanning.Infrastructure.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DailyPlanning.Infrastructure.Entities
 {
     public class Project
     {
         public Project()
         {
         }
+
+        [Key]
         public int ProjectID { get; set; }
+
+        [Required]
+        [StringLength(100)]
         public string Title { get; set; }
     }
 }
--- DailyPlanning/Infrastructure/Entities/WorkItem.cs
+++ DailyPlanning/Infrastructure/Entities/WorkItem.cs
@@ -1,4 +1,6 @@
 ﻿using DailyPlanning.Infrastructure.Enums;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace DailyPlanning.Infrastructure.Entities
 {
@@ -7,10 +9,23 @@ namespace DailyPlanning.Infrastructure.Entities
         public WorkItem()
         {
         }
+
+        [Key]
         public int WorkItemID { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Title { get; set; }
+
+        [StringLength(500)]
         public string Description { get; set; }
+
+        [Required]
         public Status Status { get; set; }
+        
         public int ProjectID { get; set; }
+
+        [ForeignKey("ProjectID")]
         public virtual Project Project { get; set; }
     }
 }
