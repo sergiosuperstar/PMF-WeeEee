489fc0f6f25a2bfcb9841bc3baf079162eacc6e3
Danilo <icf.danilo.grujicic@gmail.com>
2017-08-28 10:36:54 +0200

    Added DailyPlan

---
+++ DailyPlanning/Models/DailyPlan.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DailyPlanning.Models
+{
+    public class DailyPlan
+    {
+        public DateTime Date { get; set; }
+        public ICollection<int> DayBefore { get; set; }
+
+        public ICollection<int> Today { get; set; }
+
+        public string Note { get; set; }
+
+    }
+}
