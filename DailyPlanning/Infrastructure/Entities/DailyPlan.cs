50197c78ad0726b148d9bed4fb17a040d08e048a
Danilo <icf.danilo.grujicic@gmail.com>
2017-08-28 11:29:26 +0200

    Added annotations

--- DailyPlanning/Infrastructure/Entities/DailyPlan.cs
+++ DailyPlanning/Infrastructure/Entities/DailyPlan.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,11 +8,12 @@ namespace DailyPlanning.Infrastructure.Entities
 {
     public class DailyPlan
     {
+        [Required]
         public DateTime Date { get; set; }
-        public ICollection<int> DayBefore { get; set; }
+        public ICollection<WorkItem> DayBefore { get; set; }
 
-        public ICollection<int> Today { get; set; }
-
+        public ICollection<WorkItem> Today { get; set; }
+        [StringLength(500)]
         public string Note { get; set; }
 
     }
