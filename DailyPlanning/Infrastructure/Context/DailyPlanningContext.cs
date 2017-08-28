9b7da5753c634b22e2c1021cf5b10ba3bd95778c
Alisa <alisa.macos@gmail.com>
2017-08-28 11:54:06 +0200

    Create db set for Project and WorkItem

--- DailyPlanning/Infrastructure/Context/DailyPlanningContext.cs
+++ DailyPlanning/Infrastructure/Context/DailyPlanningContext.cs
@@ -1,11 +1,16 @@
-﻿using System;
+﻿using DailyPlanning.Infrastructure.Entities;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 
 namespace DailyPlanning.Infrastructure.Context
 {
-    public class DailyPlanningContext
+    public class DailyPlanningContext : DbContext
     {
+        public DbSet<Project> Projects { get; set; }
+
+        public DbSet<WorkItem> WorkItems { get; set; }
     }
 }
