45520fb6064cd2de15e90941b1da99cd607c2558
Alisa <alisa.macos@gmail.com>
2017-08-30 08:56:39 +0200

    Add Seed data for Project and Workitem;

--- DailyPlanning/Infrastructure/Context/DailyPlanningContext.cs
+++ DailyPlanning/Infrastructure/Context/DailyPlanningContext.cs
@@ -1,10 +1,17 @@
-﻿using DailyPlanning.Infrastructure.Entities;
+﻿using DailyPlanning.Infrastructure.Database;
+using DailyPlanning.Infrastructure.Entities;
 using System.Data.Entity;
 
 namespace DailyPlanning.Infrastructure.Context
 {
     public class DailyPlanningContext : DbContext
     {
+
+        public DailyPlanningContext() : base()
+        {
+            System.Data.Entity.Database.SetInitializer(new DailyPlanningDBInitializer());
+        }
+
         public DbSet<Project> Projects { get; set; }
 
         public DbSet<WorkItem> WorkItems { get; set; }
---
+++ DailyPlanning/Infrastructure/Database/DailyPlanningDBInitializer.cs
@@ -0,0 +1,37 @@
+﻿using DailyPlanning.Infrastructure.Context;
+using DailyPlanning.Infrastructure.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace DailyPlanning.Infrastructure.Database
+{
+    public class DailyPlanningDBInitializer : DropCreateDatabaseAlways<DailyPlanningContext>
+    {
+        protected override void Seed(DailyPlanningContext context)
+        {
+            IList<Project> defaultProjects = new List<Project>();
+
+            defaultProjects.Add(new Project() { Title = "Project 1" });
+            defaultProjects.Add(new Project() { Title = "Project 2" });
+            defaultProjects.Add(new Project() { Title = "Project 3" });
+
+            foreach (Project p in defaultProjects)
+                context.Projects.Add(p);
+
+            IList<WorkItem> defaultWorkitems = new List<WorkItem>();
+
+            defaultWorkitems.Add(new WorkItem() { Title = "WorkItem 1", Description = "First WorkItem", Status = Enums.Status.TODO, ProjectID = 5 });
+            defaultWorkitems.Add(new WorkItem() { Title = "WorkItem 2", Description = "Second WorkItem", Status = Enums.Status.IN_PROGRESS, ProjectID = 6 });
+            defaultWorkitems.Add(new WorkItem() { Title = "WorkItem 3", Description = "Third WorkItem", Status = Enums.Status.DONE, ProjectID = 7 });
+
+            foreach (WorkItem witem in defaultWorkitems)
+                context.WorkItems.Add(witem);
+
+            base.Seed(context);
+        }
+
+    }
+}
