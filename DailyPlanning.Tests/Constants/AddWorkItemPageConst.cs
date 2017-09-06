384b0d779d03f92c1aef60e9cf97cfce73489a10
Alisa <alisa.macos@gmail.com>
2017-09-06 14:58:23 +0200

    Added Test Scenarios for Project and WorkItem;

---
+++ DailyPlanning.Tests/Constants/AddWorkItemPageConst.cs
@@ -0,0 +1,13 @@
+﻿namespace DailyPlanning.Tests.Constants
+{
+    class AddWorkItemPageConst
+    {
+        public const string ADD_TITLE_TEXTBOX_ID = "add-title-textbox-id";
+
+        public const string ADD_DESCRIPTION_TEXTAREA_ID = "description";
+
+        public const string ADD_PROJECT_DROPDOWN_ID = "add-project-dropdown-id";
+
+        public const string ADD_CREATE_BUTTON_ID = "add-create-button-id";
+    }
+}
