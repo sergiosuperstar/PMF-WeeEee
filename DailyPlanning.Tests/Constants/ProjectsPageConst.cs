3ddce847fad23295efc3295b2d47fc763b5a40c8
Danilo <danilogrujicicfax@gmail.com>
2017-09-08 14:53:49 +0200

    Changed buttons in view

--- DailyPlanning.Tests/Constants/ProjectsPageConst.cs
+++ DailyPlanning.Tests/Constants/ProjectsPageConst.cs
@@ -4,9 +4,9 @@
     {
         public const string INDEX_ADD_LINK_ID = "index-add-link-id";
 
-        public const string INDEX_DELETE_LINK_CLASS = "index-delete-link-class delete btn btn-default";
+        public const string INDEX_DELETE_LINK_CLASS = "index-delete-link-class delete btn btn-danger";
 
-        public const string INDEX_EDIT_LINK_CLASS = "index-edit-link-class btn btn-default";
+        public const string INDEX_EDIT_LINK_CLASS = "index-edit-link-class btn btn-info";
 
         public const string INDEX_DETAILS_LINK_CLASS = "index-details-link-class";
 
